e30c3f36de1b59756495f239a24adb4510a0a18f
Hasan_sh <hasansh343@gmail.com>
2018-10-16 15:29:59 +0200

    next deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
+  distDir: 'build',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/textables' : '',
   cssModules: true
 })
